b92201fd5419299349c107e7aff8fc88dad3cc66
Jhonatan Alexander <jhonatan.hernandez@eyss.io>
2021-12-13 02:09:43 -0400

    added offers outside of auctions and SafeERC20 where needed

--- contracts/Marketplace.sol
+++ contracts/Marketplace.sol
@@ -3,8 +3,11 @@ pragma solidity 0.8.4;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
 contract Marketplace is ERC721{
+    using SafeERC20 for IERC20;
+
     mapping(uint => string) public fileHash;// ipfs hash
     mapping(uint => uint) public authorComissionPercent;// percent of auction that goes to the author
     mapping(uint => address) public author;// this separates the author from the owner for commission purposes
@@ -18,8 +21,17 @@ contract Marketplace is ERC721{
         address highestBidder;
         uint256 startTime;
     }
+    struct Offer {
+        bool bought;
+        bool cancelled;
+        uint256 offerAmount;
+        address buyer;
+    }
 
     mapping(uint256 => Auction) public tokenIdToAuction;
+    mapping(uint => Offer[]) public offersPerNFT;
+
+    event OfferMade(uint256 tokenId, uint256 amount);
 
     constructor(address _paymentToken) ERC721("Glimpse", "GLMS"){
         paymentToken = IERC20(_paymentToken);
@@ -35,7 +47,34 @@ contract Marketplace is ERC721{
         counter++;
         return counter - 1;
     }
+    function offer(uint256 tokenId, uint256 amount) external {
+        Offer memory o = Offer(
+            false, // bought
+            false, // cancelled
+            amount, // offerAmount
+            msg.sender // buyer
+        );
+        offersPerNFT[tokenId].push(o);
+        emit OfferMade(tokenId, amount);
+    }
+    function acceptOffer(uint tokenId, uint offerIndex) external{ // only NFT owner
+        require(msg.sender == ownerOf(tokenId));
+        Offer storage offerToAccept = offersPerNFT[tokenId][offerIndex];
+        require(!offerToAccept.bought, "offer must not have been already accepted");
+        require(!offerToAccept.cancelled, "offer must not have been cancelled");
+        offerToAccept.bought = true;
+        paymentToken.safeTransferFrom(offerToAccept.buyer, msg.sender, offerToAccept.offerAmount);
+        _transfer(msg.sender, offerToAccept.buyer, tokenId);
 
+    }
+    function cancelOffer(uint tokenId, uint offerIndex) external{ // only buyer
+        Offer storage offerToReject = offersPerNFT[tokenId][offerIndex];
+        require(!offerToReject.cancelled, "offer must not have been cancelled already");
+        offerToReject.cancelled = true;
+    }
+    function getOffers(uint tokenId) external view returns(Offer [] memory){
+        return offersPerNFT[tokenId];
+    }
     function createAuction(uint256 _tokenId, uint128 _startingPrice) external {
         require(_isApprovedOrOwner(msg.sender, _tokenId), "Marketplace: auction caller is not owner nor approved");
         address owner = ownerOf(_tokenId);
@@ -50,22 +89,17 @@ contract Marketplace is ERC721{
     }
     function bid(uint256 _tokenId, uint256 amount) external {
         require(amount > tokenIdToAuction[_tokenId].highestBid );
-        paymentToken.transferFrom(msg.sender, address(this), amount);
+        paymentToken.safeTransferFrom(msg.sender, address(this), amount);
         if (tokenIdToAuction[_tokenId].highestBidder != address(0)) {
-            paymentToken.transfer(tokenIdToAuction[_tokenId].highestBidder, tokenIdToAuction[_tokenId].highestBid);
+            paymentToken.safeTransfer(tokenIdToAuction[_tokenId].highestBidder, tokenIdToAuction[_tokenId].highestBid);
         }
         tokenIdToAuction[_tokenId].highestBidder = msg.sender;
         tokenIdToAuction[_tokenId].highestBid = amount;
     }
     function cancelAuction(uint256 _tokenId) external {
         require(tokenIdToAuction[_tokenId].seller == msg.sender, "Marketplace: only owner can cancel auction");
+        require(tokenIdToAuction[_tokenId].highestBidder == address(0), "can't cancel if there's a bid");
         _transfer(address(this), msg.sender, _tokenId);
-        if (tokenIdToAuction[_tokenId].highestBidder != address(0)) {
-            paymentToken.transfer(
-                tokenIdToAuction[_tokenId].highestBidder,
-                tokenIdToAuction[_tokenId].highestBid
-                );
-        }
         delete tokenIdToAuction[_tokenId];
     }
     function claimWinningBid(uint _tokenId) external{
@@ -73,17 +107,17 @@ contract Marketplace is ERC721{
         require(block.timestamp > tokenIdToAuction[_tokenId].startTime + 24 hours, "Marketplace: Auction time must pass");
         _transfer(address(this), msg.sender, _tokenId);
         if(tokenIdToAuction[_tokenId].seller == author[_tokenId]){
-            paymentToken.transfer(
+            paymentToken.safeTransfer(
                 tokenIdToAuction[_tokenId].seller,
                 tokenIdToAuction[_tokenId].highestBid
             );
         }else{
             uint authorComission = tokenIdToAuction[_tokenId].highestBid * 100 / authorComissionPercent[_tokenId];
-            paymentToken.transfer(
+            paymentToken.safeTransfer(
                 tokenIdToAuction[_tokenId].seller,
                 tokenIdToAuction[_tokenId].highestBid - authorComission
             );
-            paymentToken.transfer(
+            paymentToken.safeTransfer(
                 author[_tokenId],
                 authorComission
             );
