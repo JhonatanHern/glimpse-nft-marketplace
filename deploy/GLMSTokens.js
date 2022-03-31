9bfc0cbed35ca8cc9e3c235335f7f5ae47634ae7
Oscar Lugo <oscaremiliolugo@gmail.com>
2022-03-31 16:01:29 +0000

    add deploy script to ERC20Token

---
+++ deploy/GLMSTokens.js
@@ -0,0 +1,14 @@
+const CONTRACT_NAME = 'TestERC20';
+
+// modify when needed
+module.exports = async ({ getNamedAccounts, deployments }) => {
+  const { deploy } = deployments;
+  const { deployer } = await getNamedAccounts();
+
+  await deploy(CONTRACT_NAME, {
+    from: deployer,
+    log: true,
+  });
+};
+
+module.exports.tags = [CONTRACT_NAME];
--- deploy/MarketPlaceDeploy.js
+++ deploy/MarketPlaceDeploy.js
@@ -1,6 +1,4 @@
 const CONTRACT_NAME = 'Marketplace';
-
-const tokenAddress = '0x57e2d63ca5131bf7c44f7639975c53142ab9a761';
 const safe = '0x42261b574358b4EE8ad3D43FB416B4D82D61CD93';
 const masterWallet = '0x1c9458660891A6C6ad27bc9e348B7C285c149014';
 
@@ -8,12 +6,14 @@ const masterWallet = '0x1c9458660891A6C6ad27bc9e348B7C285c149014';
 module.exports = async ({ getNamedAccounts, deployments }) => {
   const { deploy } = deployments;
   const { deployer } = await getNamedAccounts();
+  const Token = await deployments.get('TestERC20');
 
   await deploy(CONTRACT_NAME, {
     from: deployer,
     log: true,
-    args: [tokenAddress, 10, safe, masterWallet],
+    args: [Token.address, 10, safe, masterWallet],
   });
 };
 
 module.exports.tags = [CONTRACT_NAME];
+module.exports.dependencies = ['TestERC20'];
